f3eee5b288a81db0e70c2d75e27a8d93cba59f5f
Dimitri LALLEMENT <dimitri.lallement@cnes.fr>
2019-04-10 17:33:20 +0200

    [Add] input file manager

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ Solver for the knapsack problem with computed cost for each element.
 ## Installation
 * Rename 'cost-sample.py' to 'cost.py'
 Fill the file with your own cost function.
-You can also rewrite the associated tests ('/tests/test_cost_sample.py').
+You can also rewrite the associated tests ('/tests/test_cost_sample.py'). 
 
 ## Usage
 
 '''python
-python3 main.py
+python3 main.py -i <inputfile>
 '''
+By default the script will take the input file '/tests/sample_data.json'
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 from solver.cost import cost_function
+import json
+import sys, getopt
 
-def main():
+INPUT_FILE_PATH = "tests/sample_data.json"
+USAGE = "main.py -i <inputfile>"
+
+def main(argv):
     """ Main function """
     #TODO implement and call solver
+    input_file = INPUT_FILE_PATH
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["inputfile="])
+
+    except getopt.GetoptError:
+        #command line syntax errors
+        print(USAGE)
+        sys.exit(2)
+
+    #Options management    
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile>')
+            sys.exit()
+        elif opt in ("-i", "--inputfile"):
+            input_file = arg
     
+    input_reader(input_file)
+
+def input_reader(input_file_path):
+    with open(input_file_path) as f:
+        items = json.load(f)
+        for item in items:
+            print(item['id'])
+    
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
     
