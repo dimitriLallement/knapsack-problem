4ce020efde4ac0f8a07310be1c234097961d875c
Dimitri LALLEMENT <dimitri.lallement@cnes.fr>
2019-04-11 17:39:22 +0200

    [Add|Fix] - Add input data computation. Readme bug fixed

--- README.md
+++ README.md
@@ -3,21 +3,25 @@
 Solver for the knapsack problem with computed cost for each element.
 
 ## Installation
-* Rename 'cost-sample.py' to 'cost.py'
+
+* Rename `cost-sample.py` to `cost.py`
 Fill the file with your own cost function.
-You can also rewrite the associated tests ('/tests/test_cost_sample.py'). 
+You can also rewrite the associated tests (`/tests/test_cost_sample.py`).
 
 ## Usage
 
-'''python
-python3 main.py -i <inputfile>
-'''
-By default the script will take the input file '/tests/sample_data.json'
+```python
+    python3 main.py -i <inputfile> -c <capacities>
+```
+
+By default the script will take the input file `/tests/sample_data.json` and the capacities `18`.
 
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 import sys, getopt
 
 INPUT_FILE_PATH = "tests/sample_data.json"
-USAGE = "main.py -i <inputfile>"
+CAPACITIES = 18
+USAGE = "main.py -i <inputfile> -c <capacities>"
 
-def main(argv):
+def main(argv, input_file = INPUT_FILE_PATH, capacities = CAPACITIES):
     """ Main function """
     #TODO implement and call solver
-    input_file = INPUT_FILE_PATH
     try:
-        opts, args = getopt.getopt(argv,"hi:",["inputfile="])
+        opts, args = getopt.getopt(argv,"hi:c",["help","inputfile=", "capacities="])
 
     except getopt.GetoptError:
         #command line syntax errors
@@ -19,20 +19,34 @@ def main(argv):
 
     #Options management    
     for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile>')
+        if opt in ("-h", "--help"):
+            print("test.py -i <inputfile>")
             sys.exit()
         elif opt in ("-i", "--inputfile"):
             input_file = arg
-    
-    input_reader(input_file)
+        elif opt in ("-c", "--capacities"):
+            capacities = arg
+
+    data = input_reader(input_file)
+
 
 def input_reader(input_file_path):
+    """Read the input data file and create the computed data structure.
+    :param inputs: input data file path
+	:return data: the matrix of the input data with computed value
+    """
+    ids = []
+    weights = []
+    values = []
     with open(input_file_path) as f:
         items = json.load(f)
         for item in items:
-            print(item['id'])
-    
+            ids.append(item["id"])
+            weights.append(item["weight"])
+            # TODO modifiy the parameters of the cost_functions
+            values.append(cost_function(item["x"], item["y"], item["z"]))
+    data = [ids, weights, values]
+    print(data)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
